dabfd9c17dc795a5a1e63576012f79536088adc3
PalakGupta <PalakGupta@LININDAUG23-266>
2023-11-15 18:33:44 +0530

    Senior/signup/speak

---
+++ Scripts/Senior_TestCases/Senior_Signup_Speak/Script1700051369082.groovy
@@ -0,0 +1,173 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import javax.sound.sampled.AudioInputStream as AudioInputStream
+import javax.sound.sampled.AudioSystem as AudioSystem
+import javax.sound.sampled.Clip as Clip
+import org.openqa.selenium.By as By
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.chrome.ChromeDriver as ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions as ChromeOptions
+import org.openqa.selenium.remote.DesiredCapabilities as DesiredCapabilities
+import org.openqa.selenium.support.ui.ExpectedConditions as ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait as WebDriverWait
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import org.openqa.selenium.WebElement as WebElement
+import org.openqa.selenium.JavascriptExecutor as JavascriptExecutor
+import com.kms.katalon.core.webui.common.WebUiCommonHelper as WebUiCommonHelper
+
+System.setProperty('webdriver.chrome.driver', 'C:/Users/PalakGupta/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe')
+
+ChromeOptions options = new ChromeOptions()
+
+options.addArguments('start-maximized')
+
+//Set preferences to allow microphone access
+options.setExperimentalOption("prefs",
+	new HashMap<String, Object>() {{
+		put("profile.default_content_setting_values.media_stream_mic", 1);
+	}}
+);
+
+// Set preferences to allow microphone access
+//options.setExperimentalOption('prefs', new script1699965124028$1())
+DesiredCapabilities capabilities = DesiredCapabilities.chrome()
+
+capabilities.setCapability(ChromeOptions.CAPABILITY, options)
+
+WebDriver driver = new ChromeDriver(capabilities)
+
+// Assuming you are using the Katalon Framework for the following lines
+DriverFactory.changeWebDriver(driver)
+
+//Open Browser
+WebUI.navigateToUrl('https://caring-connections-qa.azurewebsites.net/')
+
+//Enter UserName
+WebUI.openBrowser('https://caring-connections-qa.azurewebsites.net/')
+
+WebUI.click(findTestObject('Login_Pages/Sign_In_Page/button_Sign_Up'))
+
+WebUI.setText(findTestObject('Login_Pages/Sign_Up_Page/Input_First_Last_Name'), 'Danville')
+
+WebUI.setText(findTestObject('Login_Pages/Sign_Up_Page/Input_ZIP_code'), '464646')
+
+WebUI.setText(findTestObject('Login_Pages/Sign_Up_Page/Input_Username'), 'pgpalak107@gmail.com')
+
+WebUI.setText(findTestObject('Login_Pages/Sign_Up_Page/Input_Password'), 'Testing@123')
+
+WebUI.click(findTestObject('Login_Pages/Sign_Up_Page/input_I am a_role'))
+
+WebUI.click(findTestObject('Login_Pages/Sign_Up_Page/button_Sign_Up'))
+
+//Verify point-3 "Expectations from Companion"
+//WebUI.verifyElementPresent(findTestObject('Profile_Creation_Page/Profile_Creation_Page_Objects/Text_Expectations_From_Companion'),  10)
+//Verify Text "Select the Button of your choice"
+WebUI.verifyElementPresent(findTestObject('Profile_Creation_Page/Profile_Creation_Page_Objects/Text_Select the button of your choice to create your profile'), 
+    10)
+
+//Verify Text "We are exicted"
+WebUI.verifyElementPresent(findTestObject('Profile_Creation_Page/Profile_Creation_Page_Objects/Text_We are excited'), 10)
+
+//click on I want to speak
+//WebUI.waitForElementClickable(findTestObject('Profile_Creation_Page/Profile_Creation_Page_Objects/btn_I_Want_To_Speak'), 
+//    67)
+WebUI.waitForElementClickable(findTestObject('Profile_Creation_Page/Profile_Creation_Page_Objects/btn_Click_Record'), 67)
+
+WebUI.click(findTestObject('Profile_Creation_Page/Profile_Creation_Page_Objects/btn_Click_Record'))
+
+try {
+    // Load the audio file
+    AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File('C:/Users/PalakGupta/Downloads/Arjun Roy.wav'))
+
+    // Create a Clip and play the audio
+    Clip clip = AudioSystem.getClip()
+
+    clip.open(audioInputStream)
+
+    clip.start()
+
+    // Wait for the audio to finish playing
+    while (clip.isRunning()) {
+        Thread.sleep(1000 // Wait for 100 milliseconds)
+            )
+    }
+    
+    clip.stop()
+}
+catch (Exception e) {
+    e.printStackTrace()
+} 
+
+WebUI.click(findTestObject('Profile_Creation_Page/Profile_Creation_Page_Objects/btn_Stop'))
+
+/*
+// Find the element to which you want to scroll
+WebElement elementToScrollTo = WebUiCommonHelper.findWebElement(findTestObject('Profile_Creation_Page/Profile_Creation_Page_Objects/button_Next'), 10)
+ 
+// Scroll to the element
+JavascriptExecutor js = (JavascriptExecutor) driver
+js.executeScript("arguments[0].scrollIntoView(true);", elementToScrollTo)
+*/
+//scroll to next btn
+WebUI.scrollToElement(findTestObject('Profile_Creation_Page/Profile_Creation_Page_Objects/button_Next'), 0)
+
+WebUI.waitForElementClickable(findTestObject('Profile_Creation_Page/Profile_Creation_Page_Objects/button_Next'), 60)
+
+//click on next btn
+WebUI.click(findTestObject('Profile_Creation_Page/Profile_Creation_Page_Objects/button_Next'))
+
+Thread.sleep(40000)
+
+WebUI.waitForElementClickable(findTestObject('Category_View_Page/Category_View_Page_Objects/Input_Edit_Educational_Info'), 
+    180)
+
+String Edu = WebUI.getAttribute(findTestObject('Category_View_Page/Category_View_Page_Objects/Input_Edit_Educational_Info'), 
+    'value')
+
+WebUI.comment(Edu)
+
+String Hobbies = WebUI.getAttribute(findTestObject('Category_View_Page/Category_View_Page_Objects/Input_Edit_Appropriate_Hobbies'), 
+    'value')
+
+WebUI.comment(Hobbies)
+
+String Expect = WebUI.getAttribute(findTestObject('Category_View_Page/Category_View_Page_Objects/Input_Edit_expectation'), 
+    'value')
+
+WebUI.comment(Expect)
+
+String others = WebUI.getAttribute(findTestObject('Category_View_Page/Category_View_Page_Objects/Input_Edit_Appropriate_Health'), 
+    'value')
+
+WebUI.comment(others)
+
+//scroll to save and continue btn
+WebUI.scrollToElement(findTestObject('Object Repository/Category_View_Page/Category_View_Page_Objects/btn_Save_n_Continue'), 
+    0)
+
+Thread.sleep(10000)
+
+//click on save and continue btn
+WebUI.click(findTestObject('Object Repository/Category_View_Page/Category_View_Page_Objects/btn_Save_n_Continue'))
+
+//verify thankyou page
+WebUI.verifyElementPresent(findTestObject('Object Repository/Thankyou_Page/Thankyou_Page_Objects/txt_h1_Thank you'), 0)
+
+WebUI.closeBrowser()
+
